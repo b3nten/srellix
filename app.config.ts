e6f457217d16ed0f610d0911e4a5d998f7f9f0d7
benten <ben@benten.ca>
2024-03-10 01:01:51 -0700

    get fly working

--- app.config.ts
+++ app.config.ts
@@ -2,6 +2,14 @@ import { defineConfig } from "@solidjs/start/config";
 import unocss from "unocss/vite"
 
 export default defineConfig({
+  ssr: false,
+  server: {
+    esbuild: {
+      options: {
+        target: "esnext",
+      }
+    }
+  },
   vite: {
     plugins: [
       unocss()
